8f9fa344a297eec208d111857092fb6c2f46a5c1
Ricardo Presilla <ricardopresilla@gmail.com>
2022-10-24 12:28:18 -0300

    Add test and resources

---
+++ src/test/java/com/alfaCentauri/data/PersonaDAOTest.java
@@ -0,0 +1,30 @@
+package com.alfaCentauri.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaDAOTest {
+
+    @org.junit.jupiter.api.Test
+    void insertar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void actualizar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void eliminar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buscarPorId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buscar() {
+    }
+}
---
+++ src/test/java/com/alfaCentauri/test/ClienteEntidadPersona.java
@@ -0,0 +1,32 @@
+package com.alfaCentauri.test;
+
+import com.alfaCentauri.domain.Persona;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class ClienteEntidadPersona {
+    static Logger log = LogManager.getRootLogger();
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersonaPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        //Inicia la transaccion
+        tx.begin();
+        //No se debe especificar el ID de la base de datos
+        Persona persona1 = new Persona("Maria","Gutierrez","mgutierrez@mail.com","88991199");
+        log.debug("Objeto a persistir:" + persona1);
+        //Persistimos el objeto
+        em.persist(persona1);
+        //terminamos la transaccion
+        tx.commit();
+        log.debug("Objeto persistido" + persona1);
+        em.close();
+    }
+}
