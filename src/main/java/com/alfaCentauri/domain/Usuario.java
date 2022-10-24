fb2a84d3e55d8b88b06e83d7245bb7fecb517ee2
Ricardo Presilla <presilla.r@opendevpro.com>
2022-10-24 12:13:30 -0300

    Update entity

--- src/main/java/com/alfaCentauri/domain/Usuario.java
+++ src/main/java/com/alfaCentauri/domain/Usuario.java
@@ -1,6 +1,14 @@
 package com.alfaCentauri.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String usuario;
     private String password;
